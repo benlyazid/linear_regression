285a4f87aea17a3facc8576b21179bbda105b0b3
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2021-01-11 08:06:42 +0100

    read data from scv file

---
+++ main.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# INITIALE VALUE FOR THE EQUATION 'Y = aX +b'
+a = 0.1
+b = 100
+my_data = np.genfromtxt('data.csv', delimiter= ',')
+
+x = my_data[:, 0].reshape(-1, 1) # set the size of the array
+ones = np.ones([x.shape[0], 1]) # Create matrix(100, 1) contining 1, X.shape --> (100,1) shape[0] == 100
+x = np.concatenate([ones, x], axis=1 ) # CREATE MATRIX(100, 2) BY concatenate THE ONE and THE X
+#print(x)
+y = my_data[:, 1].reshape(-1,1) # create the y matrix
+#print(y)
+plt.scatter(my_data[:, 0].reshape(-1,1), y)
+plt.show()
