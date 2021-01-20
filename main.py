b2406e01a491f188645119d862964b2b78300424
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2021-01-20 12:49:38 +0100

    finish proj

--- main.py
+++ main.py
@@ -6,15 +6,29 @@ my_data = np.genfromtxt('data.csv', delimiter= ',')
 
 #x = my_data[:, 0].reshape(-1,1)
 #y = my_data[:, 1].reshape(-1, 1)
-x = [1, 2, 3]
-y= [3, 7, 8]
+x = [100, 12, 13]
+y= [30, 60, 19]
 print(x)
 print('----------------')
 print(y)
 print('---------------')
 x_bar = (average(x))
 y_bar = (average(y))
+base = 0
+base_2 = 0
+for i in range(len(x)):
+	s = (x[i] - x_bar) * (y[i] - y_bar)
+	base += s
+	base_2 += (x[i] - x_bar)**2
+m = base / base_2
+b = y_bar - (m * x_bar)
+
+x_0 = 0
+y_0 = m * x_0 + b
+x_1 = 1
+y_1 = m * x_1 + b
 print(x_bar, y_bar)
 plt.scatter(x, y)
-plt.axline((0, 1), (2, 6))
+
+plt.axline((x_0, y_0), (x_1, y_1))
 plt.show()
